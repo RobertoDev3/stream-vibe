f9ebea6fc1895677f89aa04fc7e8f46fe2fd8f37
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-26 19:49:08 -0300

    feat: enhance Home page layout and update button styles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,25 @@
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main className='flex h-dvh items-center justify-center'>
-      <div>página inicial</div>
+    <main className='mx-auto max-w-7xl px-4 md:px-20 xl:px-[162px]'>
+      <header className='space-y-10 pt-40 text-center'>
+        <h1 className='text-5xl font-bold'>
+          A melhor experiência de streaming
+        </h1>
+        <p className='text-sm text-[var(--grey60)]'>
+          StreamVibe é a melhor experiência de streaming para assistir seus
+          filmes e series, a qualquer hora, em qualquer lugar. Com o StreamVibe,
+          você pode desfrutar de uma ampla variedade de conteúdo, incluindo
+          filmes clássicos, series populares e muito mais. Você também pode
+          criar suas próprias listas de observação, para encontrar facilmente o
+          conteúdo que deseja assistir.
+        </p>
+        <Link href='/' className={buttonVariants({ variant: 'default' })}>
+          Comece a Assistir Agora
+        </Link>
+      </header>
     </main>
   );
 }
