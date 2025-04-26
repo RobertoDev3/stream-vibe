fe6a13bcb63f977bb3ecaab7121e245fca7d556b
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-26 19:22:33 -0300

    feat: add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className='flex h-dvh flex-col items-center justify-center gap-2'>
+      <h2 className='text-3xl font-bold'>Página não encontrada!</h2>
+      <p>Verifique o endereço digitado ou entre em contato com o suporte.</p>
+    </main>
+  );
+}
