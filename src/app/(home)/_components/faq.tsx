da1788dcb61a5e6769a90044246b946fb58c3f2b
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-28 17:58:18 -0300

    feat: integrate Accordion component into FAQ section with multiple items

--- src/app/(home)/_components/faq.tsx
+++ src/app/(home)/_components/faq.tsx
@@ -1,5 +1,11 @@
 import { Title } from '@/components/common/title';
 import { Container } from '@/components/layout/container';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@radix-ui/react-accordion';
 
 export function FAQ() {
   return (
@@ -8,7 +14,28 @@ export function FAQ() {
         title='Perguntas frequentes'
         description='Tem perguntas? Temos respostas! Confira nossa seção de perguntas frequentes para encontrar respostas para as perguntas mais comuns sobre StreamVibe.'
       />
-      <p>Hello world</p>
+      <Accordion type='single' collapsible className='w-full'>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It&apos;s animated by default, but you can disable it if you
+            prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </Container>
   );
 }
