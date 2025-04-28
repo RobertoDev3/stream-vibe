059f08e169e897f8553eb9f0846dae6f05ea1ea4
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-28 00:53:31 -0300

    fix: update clip-path to clipPath in FacebookIcon and LinkedinIcon components, and refactor Footer layout for consistency

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,81 +3,90 @@ import { Container } from './container';
 import { XIcon } from '@/assets/icons/x-icon';
 import { LinkedinIcon } from '@/assets/icons/linkedin-icon';
 import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
 
 export function Footer() {
   return (
     <footer className='bg-[var(--black06)]'>
-      <Container className='grid grid-cols-2 gap-7.5 pt-12 pb-5 md:pt-20 md:pb-10'>
-        <div className='space-y-4'>
-          <h2 className='font-semibold'>Início</h2>
-          <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
-            <li>Categorias</li>
-            <li>Dispositivos</li>
-            <li>Preços</li>
-            <li>FAQ</li>
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 className='font-semibold'>Filmes</h2>
-          <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
-            <li>Generos</li>
-            <li>Tendência</li>
-            <li>Lançamentos</li>
-            <li>Popular</li>
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 className='font-semibold'>Séries</h2>
-          <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
-            <li>Generos</li>
-            <li>Tendência</li>
-            <li>Lançamentos</li>
-            <li>Popular</li>
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 className='font-semibold'>Suporte</h2>
-          <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
-            <li>Contate-nos</li>
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 className='font-semibold'>Assinatura</h2>
-          <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
-            <li>Planos</li>
-            <li>Benefícios</li>
-          </ul>
-        </div>
-
-        <div className='space-y-4'>
-          <h2 className='font-semibold'>Conecte-se conosco</h2>
-          <div className='flex items-center gap-2.5'>
-            <Button
-              size='icon'
-              variant='secondary'
-              className='rounded-sm p-2.5'
-            >
-              <FacebookIcon className='size-5' />
-            </Button>
-            <Button
-              size='icon'
-              variant='secondary'
-              className='rounded-sm p-2.5'
-            >
-              <XIcon className='size-5 fill-white' />
-            </Button>
-            <Button
-              size='icon'
-              variant='secondary'
-              className='rounded-sm p-2.5'
-            >
-              <LinkedinIcon className='size-5' />
-            </Button>
+      <Container className='pt-12 pb-5 md:pt-20 md:pb-10'>
+        <div className='grid grid-cols-2 gap-7.5'>
+          <div className='space-y-4'>
+            <h2 className='font-semibold'>Início</h2>
+            <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
+              <li>Categorias</li>
+              <li>Dispositivos</li>
+              <li>Preços</li>
+              <li>FAQ</li>
+            </ul>
           </div>
+
+          <div className='space-y-4'>
+            <h2 className='font-semibold'>Filmes</h2>
+            <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
+              <li>Generos</li>
+              <li>Tendência</li>
+              <li>Lançamentos</li>
+              <li>Popular</li>
+            </ul>
+          </div>
+
+          <div className='space-y-4'>
+            <h2 className='font-semibold'>Séries</h2>
+            <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
+              <li>Generos</li>
+              <li>Tendência</li>
+              <li>Lançamentos</li>
+              <li>Popular</li>
+            </ul>
+          </div>
+
+          <div className='space-y-4'>
+            <h2 className='font-semibold'>Suporte</h2>
+            <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
+              <li>Contate-nos</li>
+            </ul>
+          </div>
+
+          <div className='space-y-4'>
+            <h2 className='font-semibold'>Assinatura</h2>
+            <ul className='space-y-2 text-sm font-medium text-[var(--grey60)]'>
+              <li>Planos</li>
+              <li>Benefícios</li>
+            </ul>
+          </div>
+
+          <div className='space-y-4'>
+            <h2 className='font-semibold'>Conecte-se conosco</h2>
+            <div className='flex items-center gap-2.5'>
+              <Button
+                size='icon'
+                variant='secondary'
+                className='rounded-sm p-2.5'
+              >
+                <FacebookIcon className='size-5' />
+              </Button>
+              <Button
+                size='icon'
+                variant='secondary'
+                className='rounded-sm p-2.5'
+              >
+                <XIcon className='size-5 fill-white' />
+              </Button>
+              <Button
+                size='icon'
+                variant='secondary'
+                className='rounded-sm p-2.5'
+              >
+                <LinkedinIcon className='size-5' />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-2.5'>
+          <Separator />
+          <p className='text-sm text-[var(--grey60)]'>
+            ©2025 StreamVibe. Todos os direitos reservados.
+          </p>
         </div>
       </Container>
     </footer>
