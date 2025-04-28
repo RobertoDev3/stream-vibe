a71177a4adc926ff65667fc8ba3ff0cc736d2005
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-28 12:39:20 -0300

    feat: implement Devices component to display streaming experience across multiple devices

--- src/app/(home)/_components/devices.tsx
+++ src/app/(home)/_components/devices.tsx
@@ -1,5 +1,16 @@
+import { Title } from '@/components/common/title';
 import { Container } from '@/components/layout/container';
 
 export function Devices() {
-  return <Container>Hello World</Container>;
+  return (
+    <Container>
+      <Title
+        title='Nós fornecemos experiência de streaming em vários dispositivos.'
+        description='Com o StreamVibe, você pode apreciar seus filmes e Séries favoritas a
+        qualquer hora, em qualquer lugar. Nossa plataforma foi projetada para
+        ser compatível com uma ampla gama de dispositivos, garantindo que você
+        nunca perca um momento de entretenimento.'
+      />
+    </Container>
+  );
 }
