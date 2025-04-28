e90e4b19d9870ae85d71ad6211c925c32752fdd2
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-28 12:18:57 -0300

    feat: add Devices component to Home page for enhanced functionality

---
+++ src/app/(home)/_components/devices.tsx
@@ -0,0 +1,5 @@
+import { Container } from '@/components/layout/container';
+
+export function Devices() {
+  return <Container>Hello World</Container>;
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,14 @@
 
 import { CategoriesCarousel } from '@/components/common/categories-carousel';
 import { Header } from './_components/header';
+import { Devices } from './_components/devices';
 
 export default function Home() {
   return (
     <main className='space-y-25'>
       <Header />
       <CategoriesCarousel id='categorys' />
+      <Devices />
     </main>
   );
 }
