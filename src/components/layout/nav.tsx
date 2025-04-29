4a872f1a827a4fe1d9991972f50334f2c5d43c7e
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-28 23:36:09 -0300

    feat: refactor Nav component to improve link styling and structure in navigation items

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -44,16 +44,18 @@ export function Nav() {
         <nav className='rounded-lg border-3 border-[var(--black12)] bg-[var(--black06)]'>
           <ul className='flex h-full items-center gap-4 px-4 py-2'>
             {navItems.map(({ name, path }, key) => (
-              <li
-                key={key}
-                className={cn(
-                  'py-3 text-sm text-[var(--grey75)] transition-all',
-                  'hover:rounded-md hover:bg-[var(--black10)] hover:px-5 hover:font-medium hover:text-white',
-                  pathname === path &&
-                    'rounded-md bg-[var(--black10)] px-5 py-3 font-medium text-white',
-                )}
-              >
-                <Link href={path}>{name}</Link>
+              <li key={key} className='py-3'>
+                <Link
+                  href={path}
+                  className={cn(
+                    'py-3 text-sm text-[var(--grey75)] transition-all',
+                    'hover:rounded-md hover:bg-[var(--black10)] hover:px-5 hover:font-medium hover:text-white',
+                    pathname === path &&
+                      'rounded-md bg-[var(--black10)] px-5 py-3 font-medium text-white',
+                  )}
+                >
+                  {name}
+                </Link>
               </li>
             ))}
           </ul>
