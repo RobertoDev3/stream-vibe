b03aa4345f819c61c2eca19dc773df398359214d
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-30 00:05:49 -0300

    feat: restructure pricing plans to separate monthly and annual options with improved layout

--- src/components/layout/pricing.tsx
+++ src/components/layout/pricing.tsx
@@ -3,42 +3,65 @@ import { Button } from '../ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
 import { Container } from './container';
 
+type PlanType = {
+  monthly: PlanProps[];
+  annual: PlanProps[];
+};
+
 type PlanProps = {
   name: string;
   description: string;
   price: string;
-  period: string;
 };
 
-const plans: PlanProps[] = [
-  {
-    name: 'Plano Básico',
-    description:
-      'Desfrute de uma extensa biblioteca de filmes e séries, apresentando uma variedade de conteúdo.',
-    price: 'R$25,00',
-    period: '/mês',
-  },
-  {
-    name: 'Plano Padrão',
-    description:
-      'Acesso a uma seleção mais ampla de filmes e séries, incluindo a maioria dos novos lançamentos e conteúdo exclusivo.',
-    price: 'R$45,00',
-    period: '/mês',
-  },
-  {
-    name: 'Plano Premium',
-    description:
-      'Acesso a uma seleção mais ampla de filmes e séries, incluindo todos os novos lançamentos e visualização offline.',
-    price: 'R$75,00',
-    period: '/mês',
-  },
-];
+const plans: PlanType = {
+  monthly: [
+    {
+      name: 'Plano Básico',
+      description:
+        'Desfrute de uma extensa biblioteca de filmes e séries, apresentando uma variedade de conteúdo.',
+      price: 'R$25,00',
+    },
+    {
+      name: 'Plano Padrão',
+      description:
+        'Acesso a uma seleção mais ampla de filmes e séries, incluindo a maioria dos novos lançamentos e conteúdo exclusivo.',
+      price: 'R$45,00',
+    },
+    {
+      name: 'Plano Premium',
+      description:
+        'Acesso a uma seleção mais ampla de filmes e séries, incluindo todos os novos lançamentos e visualização offline.',
+      price: 'R$75,00',
+    },
+  ],
+  annual: [
+    {
+      name: 'Plano Básico',
+      description:
+        'Desfrute de uma extensa biblioteca de filmes e séries, apresentando uma variedade de conteúdo.',
+      price: 'R$250,00',
+    },
+    {
+      name: 'Plano Padrão',
+      description:
+        'Acesso a uma seleção mais ampla de filmes e séries, incluindo a maioria dos novos lançamentos e conteúdo exclusivo.',
+      price: 'R$450,00',
+    },
+    {
+      name: 'Plano Premium',
+      description:
+        'Acesso a uma seleção mais ampla de filmes e séries, incluindo todos os novos lançamentos e visualização offline.',
+      price: 'R$750,00',
+    },
+  ],
+};
 
 export function Pricing({ id }: { id: string }) {
   return (
     <Container id={id}>
       <Tabs
-        defaultValue='account'
+        defaultValue='monthly'
         className='flex w-full flex-col gap-y-10 lg:gap-y-15'
       >
         <div className='flex flex-col items-center justify-between gap-10 lg:flex-row lg:gap-20'>
@@ -48,13 +71,13 @@ export function Pricing({ id }: { id: string }) {
           />
           <TabsList className='h-fit rounded-md border border-[var(--black15)] bg-[var(--black06)] p-2'>
             <TabsTrigger
-              value='account'
+              value='monthly'
               className='h-fit cursor-pointer px-5 py-3 text-[var(--grey60)] data-[state=active]:bg-[var(--black12)] data-[state=active]:text-white'
             >
               Mensal
             </TabsTrigger>
             <TabsTrigger
-              value='password'
+              value='Annual'
               className='h-fit cursor-pointer px-5 py-3 text-[var(--grey60)] data-[state=active]:bg-[var(--black12)] data-[state=active]:text-white'
             >
               Anual
@@ -62,10 +85,10 @@ export function Pricing({ id }: { id: string }) {
           </TabsList>
         </div>
         <TabsContent
-          value='account'
+          value='monthly'
           className='grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3'
         >
-          {plans.map((plan, key) => (
+          {plans.monthly.map((plan, key) => (
             <div
               key={key}
               className='rounded-md border border-[var(--black15)] bg-[var(--black10)] p-10'
@@ -79,7 +102,7 @@ export function Pricing({ id }: { id: string }) {
                   <p className='text-3xl font-semibold'>
                     {plan.price}
                     <span className='text-[16px] font-medium text-[var(--grey60)]'>
-                      {plan.period}
+                      /mês
                     </span>
                   </p>
                 </div>
@@ -91,7 +114,36 @@ export function Pricing({ id }: { id: string }) {
             </div>
           ))}
         </TabsContent>
-        <TabsContent value='password'>hello world 2</TabsContent>
+        <TabsContent
+          value='Annual'
+          className='grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3'
+        >
+          {plans.annual.map((plan, key) => (
+            <div
+              key={key}
+              className='rounded-md border border-[var(--black15)] bg-[var(--black10)] p-10'
+            >
+              <div className='flex h-full flex-col gap-10'>
+                <div className='space-y-3'>
+                  <h2 className='text-xl font-bold'>{plan.name}</h2>
+                  <p className='text-[var(--grey60)]'>{plan.description}</p>
+                </div>
+                <div className='flex flex-1 items-end'>
+                  <p className='text-3xl font-semibold'>
+                    {plan.price}
+                    <span className='text-[16px] font-medium text-[var(--grey60)]'>
+                      /ano
+                    </span>
+                  </p>
+                </div>
+                <div className='grid grid-cols-2 gap-3'>
+                  <Button variant='secondary'>Teste Grátis</Button>
+                  <Button>Escolher Plano</Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </TabsContent>
       </Tabs>
     </Container>
   );
