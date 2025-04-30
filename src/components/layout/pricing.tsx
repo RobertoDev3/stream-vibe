df3d49615922a7469ee1c9308d193d8421e0a730
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-29 21:54:54 -0300

    feat: enhance Pricing component with plan details and action buttons

--- src/components/layout/pricing.tsx
+++ src/components/layout/pricing.tsx
@@ -1,6 +1,38 @@
 import { Title } from '../common/title';
+import { Button } from '../ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
 import { Container } from './container';
+
+type PlanProps = {
+  name: string;
+  description: string;
+  price: string;
+  period: string;
+};
+
+const plans: PlanProps[] = [
+  {
+    name: 'Plano Básico',
+    description:
+      'Desfrute de uma extensa biblioteca de filmes e séries, apresentando uma variedade de conteúdo.',
+    price: 'R$25,00',
+    period: '/mês',
+  },
+  {
+    name: 'Plano Padrão',
+    description:
+      'Acesso a uma seleção mais ampla de filmes e séries, incluindo a maioria dos novos lançamentos e conteúdo exclusivo.',
+    price: 'R$45,00',
+    period: '/mês',
+  },
+  {
+    name: 'Plano Premium',
+    description:
+      'Acesso a uma seleção mais ampla de filmes e séries, incluindo todos os novos lançamentos e visualização offline.',
+    price: 'R$75,00',
+    period: '/mês',
+  },
+];
 
 export function Pricing() {
   return (
@@ -26,7 +58,36 @@ export function Pricing() {
             </TabsTrigger>
           </TabsList>
         </div>
-        <TabsContent value='account'>hello world 1</TabsContent>
+        <TabsContent
+          value='account'
+          className='grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3'
+        >
+          {plans.map((plan, key) => (
+            <div
+              key={key}
+              className='rounded-md border border-[var(--black15)] bg-[var(--black10)] p-10'
+            >
+              <div className='flex h-full flex-col gap-10'>
+                <div className='space-y-3'>
+                  <h2 className='text-xl font-bold'>{plan.name}</h2>
+                  <p className='text-[var(--grey60)]'>{plan.description}</p>
+                </div>
+                <div className='flex flex-1 items-end'>
+                  <p className='text-3xl font-semibold'>
+                    {plan.price}
+                    <span className='text-[16px] font-medium text-[var(--grey60)]'>
+                      {plan.period}
+                    </span>
+                  </p>
+                </div>
+                <div className='grid grid-cols-2 gap-3'>
+                  <Button variant='secondary'>Teste Grátis</Button>
+                  <Button>Escolher Plano</Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </TabsContent>
         <TabsContent value='password'>hello world 2</TabsContent>
       </Tabs>
     </Container>
